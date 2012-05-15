91d516877d41ea24f1a1d73766603c924502a3a5
kazkonno <devkonno@gmail.com>
2012-05-15 17:10:57 +0900

    reveal update

---
+++ test/i18n/localizations.js
@@ -0,0 +1,44 @@
+/*
+	Localization strings file. To translate, copy the entire "en-US" branch, paste in as new member of the object, 
+	set its name to be the two-letter language code, and translate each string.
+	As shown, strings can include simple HTML.
+*/
+String.toLocaleString({
+	"en-US": {
+		"%l_welcome": "Welcome To",
+		"%l_coursename": "Building Native Mobile Apps with Titanium",
+		"%l_agenda_title": "AGENDA",
+		"%l_lesson1": "Course Introduction",
+		"%l_lesson2": "Building Native Mobile Apps with Titanium",
+		"%l_lesson3": "You're a Native Developer Now",
+		"%l_lesson4": "Cross-platform JavaScript Apps",
+		"%l_lesson5": "UI Fundamentals",
+		"%l_lesson6": "Working with Local Data",
+		"%l_lesson7": "Working with Remote Data",
+		"%l_lesson8": "Media and the Filesystem",
+		"%l_lesson9": "Using Location Services",
+		"%l_lesson10": "Appcelerator Cloud Services",
+		"%l_lesson11": "Integrating Web Content",
+		"%l_lesson12": "Deployment and Distribution"
+	},
+    	"ja": {
+		"%l_welcome": "Welcome To",
+		"%l_coursename": "Appcelerator Titaniumによるネイティブアプリ開発",
+		"%l_agenda1_title": "アジェンダ1",
+		"%l_agenda2_title": "アジェンダ2",
+		"%l_lesson1": "コース紹介",
+		"%l_lesson2": "Appcelerator Titaniumによるネイティブアプリ開発",
+		"%l_lesson3": "これからはネイティブアプリ開発者です。",
+		"%l_lesson4": "クロスプラットフォームJavaScriptアプリ",
+		"%l_lesson5": "Titanium Studio",
+		"%l_lesson6": "UIの基礎",
+		"%l_lesson7": "ローカルデータ",
+		"%l_lesson8": "リモートデータ",
+		"%l_lesson9": "メディアとファイルシステム",
+		"%l_lesson10": "位置情報サービスの利用",
+		"%l_lesson11": "Appceleratorクラウドサービス",
+		"%l_lesson12": "Webコンテンツとの統合",
+		"%l_lesson13": "デプロイと配布"
+	}
+
+});
